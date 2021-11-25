9f44a45fea332259ae767f2c9ee760d9ed9980db
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-11-25 20:11:08 +0530

    Add files via upload

---
+++ chitchat/speech.js
@@ -0,0 +1,13 @@
+// Text to Speech
+
+const synth = window.speechSynthesis;
+
+const textToSpeech = (string) => {
+  let voice = new SpeechSynthesisUtterance(string);
+  voice.text = string;
+  voice.lang = "en-IND";
+  voice.volume = 1;
+  voice.rate = 1;
+  voice.pitch = 2; // Can be 0, 1, or 2
+  synth.speak(voice);
+}
